15892bcae2d083d25fd9b5622ded7e205f98a715
ruslanashaari <ruslan.ashaari@gmail.com>
2017-09-20 17:09:39 +0800

    fix wrong answer

--- Prob#3-LargestPrimeFactor/largestprimefactor_test.php
+++ Prob#3-LargestPrimeFactor/largestprimefactor_test.php
@@ -5,6 +5,6 @@ require 'largestprimefactor.php';
 class PrimeFactor extends PHPUnit\Framework\TestCase {
 
 	public function finding largestprimefactorof13195() {
-		$this->assertEquals(5713, largestprimefactor(13195));
+		$this->assertEquals(29, largestprimefactor(13195));
 	}
 }
