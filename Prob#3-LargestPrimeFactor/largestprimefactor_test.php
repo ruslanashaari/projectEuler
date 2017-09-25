4e5e2c62bb5b847806e47d455f51d77879ac7278
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-26 06:42:01 +0800

    add expected answer for 600851475143

--- Prob#3-LargestPrimeFactor/largestprimefactor_test.php
+++ Prob#3-LargestPrimeFactor/largestprimefactor_test.php
@@ -6,5 +6,9 @@ class PrimeFactor extends PHPUnit\Framework\TestCase {
 
 	public function testfindinglargestprimefactorof13195() {
 		$this->assertEquals(29, largestprimefactor(13195));
+	} 
+
+	public function testfindinglargestprimefactorof600851475143() {
+		$this->assertEquals(6857, largestprimefactor(600851475143));
 	}
 }
