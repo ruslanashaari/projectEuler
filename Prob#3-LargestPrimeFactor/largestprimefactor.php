fe78c6c0fd2c1e070115c5c89903796a95c0e11e
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-26 06:42:34 +0800

    solve problem by using a loop and if else

--- Prob#3-LargestPrimeFactor/largestprimefactor.php
+++ Prob#3-LargestPrimeFactor/largestprimefactor.php
@@ -1,18 +1,15 @@
+
+
 <?php
 
 function largestprimefactor($number) {
 	
 	$largestprimefactor = 0;
 
-	for ($x=2; $x<$number; $x++) {
-		if ($number%$x == 0) { //is x is the factor of number
-			for ($y=2; $y<$x; $y++) { 
-				if ($x%$y != 0) //is x is a prime number
-					break;
-				else
-					$largestprimefactor = $x;					
-			}
-			
+	for ($i=3; $i<$number; $i++) { 
+		if ($number%$i == 0 && $number%2 != 0) { 
+			$largestprimefactor = $number/$i;
+			$number = $largestprimefactor;
 		}
 	}
 
