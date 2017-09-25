e1e238f1429c41a026bdc29fef06baa598cc6f80
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-26 07:07:11 +0800

    add problem 4: readme and test

---
+++ Prob#4-LargestPalindrome/largestpalindrome_test.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'largestpalindrome.php';
+
+class palindrome extends PHPUnit\Framework\TestCase {
+	public function testfindinglargestpalindromefromproductoftwo2digitnumbers(){
+		$this->assertEquals(9009, largestpalindrome(2));	
+	}
+
+	// public function testfindinglargestpalindromefromproductoftwo3digitnumbers(){
+	// 	$this->assertEquals(9009, largestpalindrome(3));	
+	// }
+}
