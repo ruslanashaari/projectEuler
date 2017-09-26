f061b6b02cab4d638a612227d35b03d0f608b45b
ruslanashaari <ruslan.ashaari@gmail.com>
2017-09-26 17:58:40 +0800

    add isPalindrome fn and construct 2/3 digits product process

--- Prob#4-LargestPalindrome/largestpalindrome.php
+++ Prob#4-LargestPalindrome/largestpalindrome.php
@@ -1,5 +1,25 @@
 <?php
 
 function largestpalindrome($digit) {
-	
+	for ($i=99; $i>9; $i--) { 
+		for ($j=99; $j<9; $j--) {
+			if (isPalindrome($i*$j)) {
+				break;
+			}
+		}
+	}
+
+	return $i." ".$j;
+}
+
+function isPalindrome($number, $reverse = 0, $div = 0) {
+	$n = $number;
+
+	while ($number>0) {
+		$div = $number%10;
+		$reverse = $reverse*10 + $div;
+		$number = (int) ($number/10);
+	}
+
+	return ($reverse == $n ? true : false);
 }
