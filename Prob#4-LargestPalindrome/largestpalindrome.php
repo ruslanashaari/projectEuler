9ed798ec9b39d05e41d4bfdae3d278ae5b226215
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-27 23:11:47 +0800

    enable for dyamic number digit option

--- Prob#4-LargestPalindrome/largestpalindrome.php
+++ Prob#4-LargestPalindrome/largestpalindrome.php
@@ -1,12 +1,28 @@
 <?php
 
-function largestpalindrome($digit) {
-	$largestpalin = 0;
+function palindrome($digit, $i=0, $maxi=1) {
 
-	for ($i=999; $i>100; $i--) { 
-		for ($j=999; $j>100; $j--) {
-			if (isPalindrome($i*$j) && $i*$j > $largestpalin) {
-				$largestpalin = $i*$j;
+	for ($x=0; $x<$digit; $x++) { 
+		$i = $i.'9';
+		$maxi = $maxi.'0';
+	}
+
+	$i = (int) $i;
+	$maxi = (int) $maxi / 10 -1;
+
+	return largestpalindrome($i, $maxi);
+}
+
+function largestpalindrome($i, $maxi) {
+	
+	$largestpalin = 0;
+	$j = $i;
+	$maxj = $maxi;
+
+	for ($k=$i; $k>$maxi; $k--) { 
+		for ($l=$j; $l>$maxj; $l--) {
+			if (isPalindrome($k*$l) && $k*$l > $largestpalin) {
+				$largestpalin = $k*$l;
 			}
 		}
 	}
--- Prob#4-LargestPalindrome/largestpalindrome_test.php
+++ Prob#4-LargestPalindrome/largestpalindrome_test.php
@@ -4,10 +4,10 @@ require 'largestpalindrome.php';
 
 class palindrome extends PHPUnit\Framework\TestCase {
 	public function testfindinglargestpalindromefromproductoftwo2digitnumbers(){
-		$this->assertEquals(9009, largestpalindrome(2));	
+		$this->assertEquals(9009, palindrome(2));	
 	}
 
 	public function testfindinglargestpalindromefromproductoftwo3digitnumbers(){
-		$this->assertEquals(906609, largestpalindrome(3));	
+		$this->assertEquals(906609, palindrome(3));	
 	}
 }
