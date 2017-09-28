63da66c4c36102e797c5f9cda6e0655ab25f6762
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-28 22:44:30 +0800

    fix to test problem 5

--- Prob#5-SmallestMultiple/smallestmultiple_test.php
+++ Prob#5-SmallestMultiple/smallestmultiple_test.php
@@ -1,9 +1,14 @@
 <?php
 
-require 'smallestmultiple.php'
+require 'smallestmultiple.php';
 
-class multiple extendss PHPUnit\Framework\TestCase {
-	public function testSmallestMultiple() {
-		$this->assertEquals(2520, smallestmultiple(1, 20));
+class multiple extends PHPUnit\Framework\TestCase {
+	public function testSmallestMultiple1to10() {
+		$this->markTestSkipped();
+		$this->assertEquals(2520, smallestmultiple(1, 10));
+	}
+
+	public function testSmallestMultiple1to20() {
+		$this->assertEquals(232792560, smallestmultiple(1, 20));
 	}
 }
