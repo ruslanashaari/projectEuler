5fc289936321d68beb0bc5df8da1fd9052c78dac
ruslanashaari <ruslan.ashaari@gmail.com>
2017-09-29 17:33:08 +0800

    solve problem#6

--- Prob#6-SumSquareDifference/sumsquaredifference.php
+++ Prob#6-SumSquareDifference/sumsquaredifference.php
@@ -1,6 +1,22 @@
 <?php
 
-function sumsquaredifference($numbers) {
+function sumsquaredifference($number) {
 	
-	return $difference;
+	return squaresum($number) - sumsquare($number);
+}
+
+function squaresum($number, $squaresum=0) {
+	for ($i=1; $i<=$number; $i++) { 
+		$squaresum += $i;
+	}
+
+	return pow($squaresum, 2);
+}
+
+function sumsquare($number, $sumsquare=0) {
+	for ($i=1; $i<=$number; $i++) { 
+		$sumsquare += pow($i, 2);
+	}
+
+	return $sumsquare;
 }
