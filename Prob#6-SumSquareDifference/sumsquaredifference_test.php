66ead80348e5836a0a3c73c2b7eeccd9054b1d4a
ruslanashaari <ruslan.ashaari@gmail.com>
2017-09-29 17:34:31 +0800

    fix typo in readme and missing brackets in test problem 6

--- Prob#6-SumSquareDifference/sumsquaredifference_test.php
+++ Prob#6-SumSquareDifference/sumsquaredifference_test.php
@@ -3,13 +3,13 @@
 require 'sumsquaredifference.php';
 
 class sumsquaredifference extends PHPUnit\Framework\TestCase {
-	public function testSumSquareDiffof10n {
-		// $this->markTestSkipped();
+	public function testSumSquareDiffof10n() {
+		$this->markTestSkipped();
 		$this->assertEquals(2640, sumsquaredifference(10));
 	}
 
-	public function testSumSquareDiffof100n {
-		$this->markTestSkipped();
-		$this->assertEquals(2640, sumsquaredifference(100));
+	public function testSumSquareDiffof100n() {
+		// $this->markTestSkipped();
+		$this->assertEquals(25164150, sumsquaredifference(100));
 	}
 }
