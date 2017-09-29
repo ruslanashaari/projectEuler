6505d89da7ba8be189854e3b7a8ec881536ac001
ruslanashaari <ruslan.ashaari@gmail.com>
2017-09-29 16:12:20 +0800

    add test file for problem 6

---
+++ Prob#6-SumSquareDifference/sumsquaredifference_test.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'sumsquaredifference.php';
+
+class sumsquaredifference extends PHPUnit\Framework\TestCase {
+	public function testSumSquareDiffof10n {
+		// $this->markTestSkipped();
+		$this->assertEquals(2640, sumsquaredifference(10));
+	}
+
+	public function testSumSquareDiffof100n {
+		$this->markTestSkipped();
+		$this->assertEquals(2640, sumsquaredifference(100));
+	}
+}
