4494aac3a0ed3c11638961b3dd161add0394c707
ruslanashaari <ruslan.ashaari@gmail.com>
2020-02-11 10:44:14 +0800

    adjust test to use class

--- Prob#7-NumberofPrime/numberofprime_test.php
+++ Prob#7-NumberofPrime/numberofprime_test.php
@@ -1,41 +1,48 @@
-<?php
+	<?php
 
-require 'numberofprime.php';
+	require 'numberofprime.php';
 
-class numberofprimetest extends PHPUnit\Framework\Testcase {
+	class numberofprimetest extends PHPUnit\Framework\Testcase {
 
-	public function testfind1stofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(2, numberofprime(1));
+		public function testfind1stofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(1);
+			$this->assertEquals(2, $prime->numberofprime());
+		}
+
+		public function testfind2ndofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(2);
+			$this->assertEquals(3, $prime->numberofprime());
+		}
+
+		public function testfind3rdofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(3);
+			$this->assertEquals(5, $prime->numberofprime());
+		}
+
+		public function testfind4thofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(4);
+			$this->assertEquals(7, $prime->numberofprime());
+		}
+
+		public function testfind5thofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(5);
+			$this->assertEquals(11, $prime->numberofprime());
+		}
+
+		public function testfind6thofprime() {
+			// $this->markTestSkipped();
+			$prime = new NumberOfPrime(6);
+			$this->assertEquals(13, $prime->numberofprime());
+		}
+
+		public function testfind10001stofprime() {
+			$this->markTestSkipped();
+			$prime = new NumberOfPrime(10001);
+			$this->assertEquals(13, $prime->numberofprime(10001));
+		}
 	}
-
-	public function testfind2ndofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(3, numberofprime(2));
-	}
-
-	public function testfind3rdofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(5, numberofprime(3));
-	}
-
-	public function testfind4thofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(7, numberofprime(4));
-	}
-
-	public function testfind5thofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(11, numberofprime(5));
-	}
-
-	public function testfind6thofprime() {
-		// $this->markTestSkipped();
-		$this->assertEquals(13, numberofprime(6));
-	}
-
-	public function testfind10001stofprime() {
-		$this->markTestSkipped();
-		$this->assertEquals(13, numberofprime(10001));
-	}
-}
